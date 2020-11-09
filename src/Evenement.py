f5919ca15c93830f0e2faf9d3ed4d63028167369
Gaetan Jagorel <gaetan.jagorel@etu.univ-tours.fr>
2020-11-09 16:06:50 +0100

    Ajout de la methode ajouterEvenement

---
+++ src/Evenement.py
@@ -0,0 +1,4 @@
+class Evenement:
+
+    def __init__(self):
+        pass
--- src/Simulateur.py
+++ src/Simulateur.py
@@ -38,6 +38,21 @@ class Simulateur(metaclass=Singleton):
         self.borneSupRep = borneSupRep
         self.borneInfRep = borneInfRep
 
+    def ajouterEvenement(self, heure, evenement):
+        iterateur = len(self.echeancier) - 1 # On se positionne a la derniere ligne de l'echeancier
+        insere = False
+        while insere == False:
+            if iterateur == -1:
+                self.echeancier.insert(0, (heure, evenement))
+                insere = True
+            elif self.echeancier[iterateur][0] <= heure:
+                self.echeancier.insert(iterateur+1, (heure, evenement))
+                insere = True
+            iterateur -= 1
+
+
+
+
 
 
 
