b871d5d4bac34e4b68a418fe755e09657fbb26e4
Guo Julie <guoxiaoqing2350@gmail.com>
2020-11-09 17:08:59 +0100

    Create Evenement.py

---
+++ src/Evenement.py
@@ -0,0 +1,38 @@
+class Evenement():
+
+    def __init__(self):
+        pass
+
+    def procedure(self):
+        pass
+
+    def ArriveeBus(self):
+        pass
+
+    def ArriveeFileC(self):
+        pass
+
+    def AccesControle(self):
+        pass
+
+    def DepartControle(self):
+        pass
+
+    def ArriveeFileR(self):
+        pass
+
+    def AccesReparation(self):
+        pass
+
+    def DepartReparation(self):
+        pass
+
+    def DebutSimulation(self):
+        pass
+
+    def FinSimulation(self):
+        pass
+
+    def MajAires(self):
+        pass
+
