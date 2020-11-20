2f74a9d37bd29909d15aa10f4c7ee10e5bacaeb4
Victor <victor.chrzanowski@etu.univ-tours.fr>
2020-11-20 16:11:48 +0100

    Fin Simulation

--- src/FinSimulation.py
+++ src/FinSimulation.py
@@ -10,4 +10,7 @@ class FinSimulation(Evenement):
     def procedure(self):
         simulateur = Simulateur()
         simulateur.echeancier = []
+        simulateur.TpsAttenteMoyControle = simulateur.histo.aireQc/simulateur.nbBus
+        simulateur.TpsAttenteMoyReparation = simulateur.histo.aireQr/simulateur.nbBusRep
+        simulateur.TauxUtilisationCentrereparation = simulateur.histo.aireBr/(2*simulateur.dureeMax)
 
