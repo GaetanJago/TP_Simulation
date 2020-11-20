c98e01d27b2d217e90551bf33faa12d71222955a
YohanDerenne <yohan.derenne@gmail.com>
2020-11-20 15:50:07 +0100

    procedure de ArriveeFileR

---
+++ src/ArriveeFileR.py
@@ -0,0 +1,16 @@
+from src.Evenement import Evenement
+from src.Simulateur import Simulateur
+from src.AccesReparation import AccesReparation
+
+class ArriveeFileR(Evenement):
+
+    def __init__(self):
+        pass
+
+    def procedure(self):
+        simu = Simulateur()
+        simu.qr = simu.qr + 1
+        simu.nbBusRep = simu.nbBusRep + 1
+        if simu.br < 2 :
+            simu.ajouterEvenement(simu.dateSimu,AccesReparation())
+
